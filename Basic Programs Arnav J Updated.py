25de40f919ab208c2eb7a9a62c5839202fec65de
Arnav Jakkapalli <jakarnav@gmail.com>
2026-08-25 20:53:54 +0530

    Add files via upload

---
+++ Basic Programs Arnav J Updated.py
@@ -0,0 +1,17 @@
+
+num_1 = float(input("Enter your first number: "))
+num_2 = float(input("Enter your second number: "))
+
+print("\nChoose your operation: 1.Addition, 2.Subtraction, 3.Multiplication, 4.Division")
+choice = int(input("Enter your choice (1-4): "))
+
+if choice == 1:
+    print(num_1 + num_2)
+elif choice == 2:
+    print(num_1 - num_2)
+elif choice == 3:
+    print(num_1 * num_2)
+elif choice == 4:
+    print(num_1 / num_2)
+else:
+    print("Invalid choice", "\nRun the program again and choose a valid option.")
